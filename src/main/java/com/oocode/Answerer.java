f7d03cdc2beabcd3d9100c073be37704f4544cad
Till <Till.Seifert@d-fine.de>
2024-04-25 15:13:49 +0100

    answer plus question

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -1,11 +1,20 @@
 package com.oocode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Answerer {
     public String answerFor(String question) {
-        switch (question) {
-            case "What is your name?":
-                return "TobiTill";
+        List<String> questionList = Arrays.asList(question.split(" "));
+        if (Objects.equals(questionList.get(0), "What") && Objects.equals(questionList.get(1), "is")) {
+            return String.valueOf(Integer.parseInt(questionList.get(2)) + Integer.parseInt(questionList.get(4)));
+        } else {
+            switch (question) {
+                case "What is your name?":
+                    return "TobiTill";
 
+            }
         }
         return "57";
     }
